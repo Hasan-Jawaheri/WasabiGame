c424c03ad1a4ecbdfaa27f13fe3514fcd6179373
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2020-01-19 10:53:03 +0100

    Removed RPG/

--- include/RPG/Units/RPGUnits.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "../../WasabiGame/Units/UnitsManager.hpp"
-
-enum UNIT_TYPE {
-	UNIT_PLAYER = 0,
-};
-
-void SetupRPGUnits();
--- src/RPG/Units/RPGUnits.cpp
+++
@@ -1,9 +0,0 @@
-#include "RPGUnits.hpp"
-#include "../../WasabiGame/Units/Units.hpp"
-
-#include "Player.hpp"
-
-void SetupRPGUnits() {
-	UnitsManager::ResetUnits();
-	UnitsManager::RegisterUnit(UNIT_PLAYER, []() { return UnitsManager::CreateUnitAndAI<Player, PlayerAI>(std::string(Player::modelName)); });
-}
