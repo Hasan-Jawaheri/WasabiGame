7e75c81d1e44acc4a5649e452fbc7fd659146c6f
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2020-01-19 12:11:50 +0100

    #1 Added WasabiGame's map to namespace

--- include/WasabiGame/Maps/Map.hpp
+++ include/WasabiGame/Maps/Map.hpp
@@ -3,16 +3,21 @@
 #include "Common.hpp"
 #include "WasabiGame/Maps/MapLoader.hpp"
 
-class Map {
-public:
 
-	struct MAP_DESCRIPTION {
-		std::string mapFilename;
+namespace WasabiGame {
+
+	class Map {
+	public:
+
+		struct MAP_DESCRIPTION {
+			std::string mapFilename;
+		};
+
+		virtual MAP_DESCRIPTION GetDescription() const = 0;
+
+		virtual void OnLoaded() {}
+		virtual void Update(float fTimeDelta) {}
+		virtual void Cleanup() {}
 	};
 
-	virtual MAP_DESCRIPTION GetDescription() const = 0;
-
-	virtual void OnLoaded() {}
-	virtual void Update(float fTimeDelta) {}
-	virtual void Cleanup() {}
 };
