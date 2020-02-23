bc22b4906734d922b463357399e494603adc017a
Hasan Al-Jawaheri <hasan.aljawaheri@gmail.com>
2020-02-23 18:32:14 +0100

    Fixed the test loader

--- src/RTBTest/Main.cpp
+++ src/RTBTest/Main.cpp
@@ -9,26 +9,25 @@
 
 Wasabi* WInitialize() {
 	if (GENERATE_ASSETS) {
-		AssetGenerator AG = AssetGenerator("Media/RollTheBall");
+		RollTheBall::AssetGenerator AG = RollTheBall::AssetGenerator("Media/RollTheBall");
 		if (!AG.Generate())
 			return nullptr;
 	}
 
 	std::thread serverThread([]() {
 		std::srand(std::time(nullptr) + 3179);
-		RTBServer server;
-		server.Initialize(false);
-		server.Run();
-		server.Destroy();
+		std::shared_ptr<RTBServer::ServerApplication> server = std::make_shared<RTBServer::ServerApplication>();
+		server->Initialize(false);
+		server->Run();
+		server->Destroy();
 	});
 
 	std::vector<std::thread> clientThreads;
 	for (int i = 0; i < NUM_CLIENTS; i++) {
 		clientThreads.push_back(std::thread([i]() {
 			std::srand(std::time(nullptr) + i * 1789);
-			Wasabi* client = new RTBClient(false, false, false);
-			RunWasabi(client);
-			delete client;
+			std::shared_ptr<RTBClient::ClientApplication> client = std::make_shared<RTBClient::ClientApplication>(false, false, false);
+			RunWasabi(client.get());
 		}));
 	}
 
